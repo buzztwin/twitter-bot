e9182e32b830a72b1439aee037bc49cec8175281
buzztwin <39138882+buzztwin@users.noreply.github.com>
2018-05-11 18:04:28 +0200

    Update Twit.js

--- src/Twit.js
+++ src/Twit.js
@@ -1,14 +1,7 @@
 const Twit = require("twit");
 const config = require("../config");
 const T = new Twit(config.twitterApp);
-const Youtube = require("youtube-api")
-    , fs = require("fs")
-    , readJson = require("r-json")
-    , Lien = require("lien")
-    , Logger = require("bug-killer")
-    , opn = require("opn")
-    , prettyBytes = require("pretty-bytes")
-    ;
+
 //var google = require('googleapis');
 
 module.exports = T;
