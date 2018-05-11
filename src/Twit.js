1aa55a9d894db50af7ddc4de26d14dc884652006
buzztwin <39138882+buzztwin@users.noreply.github.com>
2018-05-11 18:16:48 +0200

    Update Twit.js

--- src/Twit.js
+++ src/Twit.js
@@ -1,6 +1,7 @@
 const Twit = require("twit");
 const config = require("../config");
 const T = new Twit(config.twitterApp);
+const Youtube = require("googleapis");
 
 //var google = require('googleapis');
 
