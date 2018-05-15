f3161ac76e7b66477bd95edacb4e80db0f40ab5e
buzztwin <39138882+buzztwin@users.noreply.github.com>
2018-05-15 18:23:01 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -222,6 +222,21 @@ const AutoDM = () => {
               });
     }  
      
+     
+       if (tweet.user.screen_name == 'Camilas1Empire')
+    {
+              T.post('favorites/create', {
+                id: tweet.id_str
+              }, (err, data, response) => {
+                  console.log(`${data.text} from HomeTimeline tweet liked!`);
+              });
+     
+               T.post('statuses/retweet/:id', {
+                id: tweet.id_str
+              }, (err, data, response) => {
+                  console.log(`${data.text} from Home Timeline tweet RT!`);
+              });
+    }  
     
     
   });
